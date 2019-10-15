870a0ba68e61fa62804b57a5f2f7939038df5631
1127071993@qq.com <aini2009626>
2019-10-15 21:13:46 +0800

    2-3 axios请求方法及别名（post和put方法）

---
+++ src/views/2-2.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+<script>
+/**
+    axios的请求方法  get post put patch delete
+
+    get 获取数据
+    post 提交数据 （表单提交+文件上传）
+    put 更新数据 （整体更新，）
+    patch 更新数据 （只更新修改了的数据）
+    delete 删除数据
+ */
+import axios from 'axios'
+export default {
+  name: 'axios2-2',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  created () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      axios.get('data.json', {
+        params: {
+          id: 123
+        }
+      }).then(res => {
+        console.log(res.data)
+        // this.msg = res.data
+      })
+
+      axios({
+        methods: 'get',
+        url: 'data.json',
+        params: { id: 456 }
+      }).then(res => {
+        console.log(res)
+      })
+
+      // post
+      // form-data 表单提交（图片、文件上传，比较老的一种方式）
+      // application/json(现在用的比较多)
+      let data = {
+        id: 1
+      }
+      axios.post('/post', data).then(res => {
+        console.log(res)
+      })
+      axios({
+        methods: 'post',
+        url: '/post',
+        data: data
+      }).then(res => {
+        console.log(res)
+      })
+
+      // form-data请求
+      let formData = new FormData()
+      for (let key in data) {
+        formData.append(key, data[key])
+      }
+      axios.post('/post', formData).then(res => {
+        console.log(res)
+      })
+
+      // put请求
+      axios.put('/put', data).then(res => {
+        console.log(res)
+      })
+      // patch请求
+      axios.patch('/patch', data).then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'about',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  created () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      axios.get('data.json').then(res => {
+        console.log(res.data)
+        // this.msg = res.data
+      })
+    }
+  }
+}
+</script>
